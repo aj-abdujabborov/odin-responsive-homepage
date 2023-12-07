0db4b2486b789c7afd36726c9d984386fbee76f8
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-12-07 18:09:24 +0500

    Build about me section for mobile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./index.scss";
 
-console.log("works!");
+const projects = [...document.querySelectorAll(".project .info h2")];
+const projectCardLinksTemplate = document.querySelector(
+  ".templates .external-links",
+);
+
+projects.forEach((node) => {
+  const inst = projectCardLinksTemplate.cloneNode(true);
+  node.after(inst);
+
+  const projectTitle = node.textContent;
+  const githubIcon = inst.querySelector("a.github-icon");
+  const newTabIcon = inst.querySelector("a.new-tab-icon");
+
+  githubIcon.setAttribute("aria-label", `See ${projectTitle} on Github`);
+  newTabIcon.setAttribute("aria-label", `See ${projectTitle}`);
+});
