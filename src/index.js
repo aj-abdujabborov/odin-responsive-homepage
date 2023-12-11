943da75b92c0d1244de70b3b951ca5218305c5b9
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-12-12 01:13:05 +0500

    Clean up code (slightly)

--- src/index.js
+++ src/index.js
@@ -1,43 +1,47 @@
 import "./styles/index.scss";
 
-const projects = [...document.querySelectorAll(".project .info h2")];
-const projectCardLinksTemplate = document.querySelector(
-  ".templates .external-links",
-);
+(function addExternalLinksToProjectCards() {
+  const projects = [...document.querySelectorAll(".project .info h2")];
+  const projectCardLinksTemplate = document.querySelector(
+    ".templates .external-links",
+  );
 
-projects.forEach((node) => {
-  const inst = projectCardLinksTemplate.cloneNode(true);
-  node.after(inst);
+  projects.forEach((node) => {
+    const inst = projectCardLinksTemplate.cloneNode(true);
+    node.after(inst);
 
-  const projectTitle = node.textContent;
-  const githubIcon = inst.querySelector("a.github-icon");
-  const newTabIcon = inst.querySelector("a.new-tab-icon");
+    const projectTitle = node.textContent;
+    const githubIcon = inst.querySelector("a.github-icon");
+    const newTabIcon = inst.querySelector("a.new-tab-icon");
 
-  githubIcon.setAttribute("aria-label", `See ${projectTitle} on Github`);
-  newTabIcon.setAttribute("aria-label", `See ${projectTitle}`);
-});
+    githubIcon.setAttribute("aria-label", `See ${projectTitle} on Github`);
+    newTabIcon.setAttribute("aria-label", `See ${projectTitle}`);
+  });
+})();
 
-// Color modes
-const colorModeButton = document.querySelector("button.power-button");
+(function setUpColorModeButton() {
+  const colorModeButton = document.querySelector("button.power-button");
 
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark-theme") {
-  document.body.classList.toggle("dark-theme");
-} else if (savedTheme === "light-theme") {
-  document.body.classList.toggle("light-theme");
-}
-
-colorModeButton.addEventListener("click", () => {
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const bodyClassList = document.body.classList;
-  if (prefersDark) {
-    bodyClassList.toggle("light-theme");
-    bodyClassList.remove("dark-theme");
-  } else {
-    bodyClassList.toggle("dark-theme");
-    bodyClassList.remove("light-theme");
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark-theme") {
+    document.body.classList.toggle("dark-theme");
+  } else if (savedTheme === "light-theme") {
+    document.body.classList.toggle("light-theme");
   }
 
-  console.log();
-  localStorage.setItem("theme", bodyClassList[0] || "");
-});
+  colorModeButton.addEventListener("click", () => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+    const bodyClassList = document.body.classList;
+    if (prefersDark) {
+      bodyClassList.toggle("light-theme");
+      bodyClassList.remove("dark-theme");
+    } else {
+      bodyClassList.toggle("dark-theme");
+      bodyClassList.remove("light-theme");
+    }
+
+    localStorage.setItem("theme", bodyClassList[0] || "");
+  });
+})();
